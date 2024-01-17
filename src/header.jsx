f09d9bd2a41967e89260dabf58e6b60a666f365d
ANIKETH VISWAM C <147573381+ANIKETH-VISWAM@users.noreply.github.com>
2024-01-17 12:59:26 +0530

    TILL LOCAL STORAGE &DELETE.

---
+++ src/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header className="App-header">
+        <p>GROCERY-LIST</p>
+       
+    </header>
+    </>
+  )
+}
+
+export default Header;

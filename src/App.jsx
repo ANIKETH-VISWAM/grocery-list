1ca31eec915e9d5d0511a5236c9684f7b656ca26
ANIKETH VISWAM C <147573381+ANIKETH-VISWAM@users.noreply.github.com>
2024-01-17 14:07:31 +0530

    destructuring using props and props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
 import Header from "./header";
 import Content from "./content";
 import Footer from "./footer";
+import { useState } from "react";
 
 
 
 
 function App() {
+  const [items,setItems]=useState([
+    {
+      id:1,
+      checked:false,
+      item:'one kilo potato'
+    },
+    {
+      id:2,
+      checked:false,
+      item:'item2'
+    },
+    {
+      id:3,
+      checked:false,
+      item:'item3'
+    }
+  ]);
+  const handleCheck =(id)=>{
+    const listItems=items.map((item)=>item.id===id ?{...item,
+      checked : !item.checked } :item);
+      setItems(listItems);
+      localStorage.setItem('shoppinglist',JSON.stringify(listItems));
+  };
+  const handleDelete = (id)=>{
+    const listItems= items.filter((item)=>item.id!==id)
+    setItems(listItems);
+    localStorage.setItem('shoppinglist',JSON.stringify(listItems));
+  }
   return (
    <div className="App">
     <Header/>
-   <Content/>
-   <Footer/>
+   <Content 
+   items={items}
+   
+   handleCheck={handleCheck}
+   handleDelete={handleDelete}
+   
+   />
+   <Footer length={items.length}/>
    </div>
    
   );
--- src/content.jsx
+++ src/content.jsx
@@ -1,53 +1,19 @@
-import  { useState } from 'react'
-import {FaTrash} from 'react-icons/fa'
 
 
-const Content = () => {
-  const [items,setItems]=useState([
-    {
-      id:1,
-      checked:false,
-      item:'one kilo potato'
-    },
-    {
-      id:2,
-      checked:false,
-      item:'item2'
-    },
-    {
-      id:3,
-      checked:false,
-      item:'item3'
-    }
-  ]);
-  const handleCheck =(id)=>{
-    const listItems=items.map((item)=>item.id===id ?{...item,
-      checked : !item.checked } :item);
-      setItems(listItems);
-      localStorage.setItem('shoppinglist',JSON.stringify(listItems));
-  };
-  const handleDelete = (id)=>{
-    const listItems= items.filter((item)=>item.id!==id)
-    setItems(listItems);
-    localStorage.setItem('shoppinglist',JSON.stringify(listItems));
-  }
+import ItemList from './ItemList'
+
+
+const Content = ({items,handleCheck,handleDelete}) => {
+  
+  
   return (
     <main>
       {items.length? (
-      <ul>
-        {
-          items.map((item)=>(
-            <li className='item' key={item.id}>
-              <input type="checkbox" onChange={()=>handleCheck(item.id)} checked={item.checked} />
-              <label 
-              style={(item.checked)?{textDecoration:'line-through'}:null}
-              onDoubleClick={()=>handleCheck(item.id)}
-              >{item.item}</label>
-              <FaTrash onClick={()=>handleDelete(item.id)} role='button' tabIndex={0}/>
-            </li>
-          ))
-        }
-      </ul>
+        <ItemList
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}/>
+      
       ) :( <p style={{marginTop:'2rem'}}>YOUR LIST IS EMPTY.</p>
       )}
 

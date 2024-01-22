84dcabe690c3bdd3e978eac6507b6cf7a8b42837
ANIKETH VISWAM C <147573381+ANIKETH-VISWAM@users.noreply.github.com>
2024-01-22 11:06:07 +0530

    DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,63 @@
 import Header from "./header";
 import Content from "./content";
 import Footer from "./footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import AddItem from "./AddItem";
 
 
 
 
 function App() {
-  const [items,setItems]=useState([
-    {
-      id:1,
-      checked:false,
-      item:'one kilo potato'
-    },
-    {
-      id:2,
-      checked:false,
-      item:'item2'
-    },
-    {
-      id:3,
-      checked:false,
-      item:'item3'
-    }
-  ]);
+  const [items,setItems]=useState(JSON.parse(localStorage.getItem('shoppinglist'))||[]);
+
+  const [newItem,setNewItem]=useState('');
+
+  useEffect(()=>{
+    localStorage.setItem('shoppinglist',JSON.stringify(items));
+
+  },[items])
+
+  
+
+  const addItem =(item)=>{
+    const id = items.length ? items[items.length-1].id+1 :1;
+    const myNewItem={id, checked:false, item};
+    const listItems=[...items,myNewItem];
+    setItems(listItems);
+  }
+
   const handleCheck =(id)=>{
     const listItems=items.map((item)=>item.id===id ?{...item,
       checked : !item.checked } :item);
-      setItems(listItems);
-      localStorage.setItem('shoppinglist',JSON.stringify(listItems));
+     setItems(listItems);
+      
   };
+
   const handleDelete = (id)=>{
     const listItems= items.filter((item)=>item.id!==id)
-    setItems(listItems);
-    localStorage.setItem('shoppinglist',JSON.stringify(listItems));
+   setItems(listItems);
+    
   }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem('')
+    
+  }
+
   return (
    <div className="App">
     <Header/>
+    <AddItem
+      
+      newItem={newItem}
+      setNewItem={setNewItem}
+      handleSubmit={handleSubmit}
+
+    
+    />
    <Content 
    items={items}
    
